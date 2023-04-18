d56f5de2378e649a10970f9d6cd67fd29a06026b
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-18 01:26:50 -0300

    Remove inactive users from users list

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -75,7 +75,7 @@ User.login = (params, result) => {
 }
 
 User.getAll = (title, result) => {
-  let query = "SELECT u.id, u.name, full_name, email, role, r.name role_name, status, CASE WHEN status=1 THEN 'Active' WHEN status=0 THEN 'Inactive' END status_name  FROM user u INNER JOIN role r ON u.role = r.id;"
+  let query = "SELECT u.id, u.name, full_name, email, role, r.name role_name, status, CASE WHEN status=1 THEN 'Active' WHEN status=0 THEN 'Inactive' END status_name  FROM user u INNER JOIN role r ON u.role = r.id WHERE status=1;"
 
   if (title) {
     query += ` WHERE title LIKE '%${title}%'`

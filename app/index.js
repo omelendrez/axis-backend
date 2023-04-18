53238cbbeeff22ad360ea828ab2b38d172650320
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-18 16:26:02 -0300

    Fix role endpoint error

--- app/index.js
+++ app/index.js
@@ -1,26 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const { log } = require("./helpers/log.js")
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { log } = require("./helpers/log.js");
+const logger = require("morgan");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Axis." })
-})
+  res.json({ message: "Welcome to Axis." });
+});
 
-require("./routes/user.routes.js")(app)
-require("./routes/trainee.routes.js")(app)
-require("./routes/role.routes.js")(app)
+require("./routes/user.routes.js")(app);
+require("./routes/trainee.routes.js")(app);
+require("./routes/role.routes.js")(app);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  log.info(`Server is running on port ${PORT}.`)
-})
+  log.info(`Server is running on port ${PORT}.`);
+});

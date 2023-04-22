87d67a7b385cbf560a9f4e1eba8b2e71157ddffa
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-21 22:53:50 -0300

    Config cors

--- app/index.js
+++ app/index.js
@@ -6,7 +6,26 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:4173',
+  'https://axis-tolmann.vercel.app'
+]
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.'
+        return callback(new Error(msg), false)
+      }
+      return callback(null, true)
+    }
+  })
+)
 
 app.use(express.json())
 

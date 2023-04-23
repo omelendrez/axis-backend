3b23e73d3a4bca15670f47ccba62d5a36d8f2ee7
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-23 08:52:25 -0300

    Activate cors

--- app/index.js
+++ app/index.js
@@ -6,32 +6,32 @@ require('dotenv').config()
 
 const app = express()
 
-// const whitelist = [
-//   // 'http://localhost:5173',
-//   'http://localhost:4173',
-//   'http://192.168.0.139:5173',
-//   'https://axis-tolmann.vercel.app'
-// ]
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true)
-//     if (whitelist.indexOf(origin) === -1) {
-//       const msg =
-//         'The CORS policy for this site does not ' +
-//         'allow access from the specified Origin.'
-//       return callback(new Error(msg), false)
-//     }
-//     return callback(null, true)
-//   }
-// }
+const whitelist = [
+  'http://localhost:5173',
+  'http://localhost:4173',
+  'http://192.168.0.139:5173',
+  'https://axis-tolmann.vercel.app'
+]
 
 const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+    if (whitelist.indexOf(origin) === -1) {
+      const msg =
+        'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  }
 }
+
+// const corsOptions = {
+//   origin: '*',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204
+// }
 
 app.use(cors(corsOptions))
 

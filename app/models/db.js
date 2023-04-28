b87dd0508b12b0796759fcf7d313769e4f274d99
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-28 12:57:08 -0300

    Make a connection pool

--- app/models/db.js
+++ app/models/db.js
@@ -2,25 +2,19 @@ require('dotenv').config()
 const { log } = require('./../helpers/log')
 const mysql = require('mysql2')
 
-// Create a connection to the database
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
   port: process.env.MYSQL_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10,
-  idleTimeout: 60000,
-  queueLimit: 0,
   multipleStatements: true
 })
 
-// open the MySQL connection
-connection.connect((error) => {
-  if (error) throw error
-  log.info('Successfully connected to the database.')
+pool.getConnection((err, connection) => {
+  if (err) throw err
+  log.info(`Successfully connected to ${process.env.MYSQL_DB} database.`)
+  connection.release()
 })
 
-module.exports = connection
+module.exports = pool

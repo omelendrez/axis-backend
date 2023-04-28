856349b7c7a98a963325f69479818be5f32e08b1
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-28 13:09:39 -0300

    Change cors and add headers

--- app/index.js
+++ app/index.js
@@ -6,32 +6,32 @@ require('dotenv').config()
 
 const app = express()
 
-const whitelist = [
-  'http://localhost:5173',
-  'http://localhost:4173',
-  'http://192.168.0.139:5173',
-  'https://axis-tolmann.vercel.app'
-]
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true)
-    if (whitelist.indexOf(origin) === -1) {
-      const msg =
-        'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.'
-      return callback(new Error(msg), false)
-    }
-    return callback(null, true)
-  }
-}
+// const whitelist = [
+//   'http://localhost:5173',
+//   'http://localhost:4173',
+//   'http://192.168.0.139:5173',
+//   'https://axis-tolmann.vercel.app'
+// ]
 
 // const corsOptions = {
-//   origin: '*',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true)
+//     if (whitelist.indexOf(origin) === -1) {
+//       const msg =
+//         'The CORS policy for this site does not ' +
+//         'allow access from the specified Origin.'
+//       return callback(new Error(msg), false)
+//     }
+//     return callback(null, true)
+//   }
 // }
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
 
 app.use(cors(corsOptions))
 
@@ -41,14 +41,25 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(
   logger('dev', {
-    skip: function (req, res) {
-      return res.statusCode < 400
-    }
+    skip: (req, res) => res.statusCode < 400
   })
 )
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Axis.' })
+})
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+  )
+  next()
 })
 
 require('./routes/user.routes.js')(app)

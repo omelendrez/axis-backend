1710aee2e545a3ad53a227e1ef8dc9a7a1bce443
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-02 13:13:50 -0300

    Fix bug deleting course item form course

--- app/models/course-item-rel.model.js
+++ app/models/course-item-rel.model.js
@@ -57,37 +57,20 @@ CourseItemRel.getAllAvailable = (id, result) => {
 }
 
 CourseItemRel.remove = (id, result) => {
-  sql.query(
-    'SELECT COUNT(1) records FROM course_item_rel WHERE item = ?;',
-    id,
-    (err, res) => {
-      if (err) {
-        log.error(err)
-        result(err, null)
-        return
-      }
-
-      if (res[0].records) {
-        result({ kind: 'cannot_delete' }, null)
-        return
-      }
-
-      sql.query('DELETE FROM course_item_rel WHERE id = ?', id, (err, res) => {
-        if (err) {
-          log.error(err)
-          result(err, null)
-          return
-        }
-
-        if (res.affectedRows === 0) {
-          result({ kind: 'not_found' }, null)
-          return
-        }
-
-        result(null, id)
-      })
+  sql.query('DELETE FROM course_item_rel WHERE id = ?', id, (err, res) => {
+    if (err) {
+      log.error(err)
+      result(err, null)
+      return
     }
-  )
+
+    if (res.affectedRows === 0) {
+      result({ kind: 'not_found' }, null)
+      return
+    }
+
+    result(null, id)
+  })
 }
 
 CourseItemRel.removeAll = (result) => {

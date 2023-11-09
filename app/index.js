110fbc965c7f47a9ae1c6dcb33c21840c21f68dd
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-09 10:12:59 -0300

    Add vercel frontend url to cors

--- app/index.js
+++ app/index.js
@@ -26,7 +26,8 @@ const whitelist = [
   'http://192.168.0.139:4173',
   'http://192.168.1.88',
   'http://192.168.0.96',
-  'http://axis2'
+  'http://axis2',
+  'https://axis-tolmann.vercel.app'
 ]
 
 const corsOptions = {

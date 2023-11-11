d21e347174e3d901ff402596b88ae148f6944417
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-11 18:50:23 -0300

    Remove unnecessary files from backupt folder

--- backup/railway_upload.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -h roundhouse.proxy.rlwy.net --port 49881 --password=21c16gef3Gcba4cF6fDB25ggeDc6gEC5 -u root --routines --column-statistics=0 --compact --log-error=upload_error.log railway < axis.sql
-pause

ad1456574e04d7cd810ee9bcfdc25bd0809c75ba
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-13 15:38:30 -0300

    update local backup

--- backup/local_backup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -h 127.0.0.1  -u root --routines --column-statistics=0 --password=M1a4$1t4E8r0 axis --result-file=axis.sql
-pause

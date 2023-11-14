37a00bf0dfac1486ae319b0ebb5df9e5e2a3034d
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-14 18:56:08 -0300

    Add backup files

--- backup/database_backup.bat
+++ backup/database_backup.bat
@@ -1,14 +1,13 @@
 @echo off
-@REM "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -h 127.0.0.1  -u root --no-data --routines --column-statistics=0 --password=M1a4$1t4E8r0 axis --result-file=axis-defs.sql
-@REM "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -h 127.0.0.1  -u root --no-create-info --routines --column-statistics=0 --password=M1a4$1t4E8r0 axis --result-file=axis-data.sql
-@REM tar -cvzf axis-defs.tar.gz axis-defs.sql
-@REM tar -cvzf axis-data.tar.gz axis-data.sql
-@REM del *.sql
 
-@REM git add .
-@REM git commit -m "Add backup files"
-@REM git push
+for /f %%F in (tables-list.txt) do "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -h 127.0.0.1  -u root --routines --column-statistics=0 --password=M1a4$1t4E8r0 axis --result-file=%%F.sql "%%F"
 
-for /d %%F in ("tables-list.tst") do call echo "%%F"
+for /f %%F in (tables-list.txt) do tar -cvzf %%F.tar.gz %%F.sql
+
+del *.sql
+
+git add .
+git commit -m "Add backup files"
+git push
 
 pause
